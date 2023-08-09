f5692130e58d75cadc585e426946d6be600d5870
rizalchris <rizalchrisdev@gmail.com>
2023-08-10 02:08:47 +0800

    Masih ngebug asuuu!

--- database.php
+++ database.php
@@ -1,17 +1,18 @@
 <?php
 
 class DatabaseInstaller {
-
-    private $sql_file_path;
     private $wpdb;
+    private $sql_file_path;
+    private $backup_file_path;  // Path to the backup file
 
     public function __construct() {
         global $wpdb;
         $this->wpdb = $wpdb;
         $this->sql_file_path = plugin_dir_path(__FILE__) . 'database/bravefactor.sql';
+        $this->backup_file_path = plugin_dir_path(__FILE__) . 'database/backup.sql';  // Assuming you've created a backup
     }
 
-    public function restore_and_replace() {
+    public function restore_database() {
         // Check if the SQL file exists
         if (!file_exists($this->sql_file_path)) {
             return "Error: SQL file doesn't exist.";
@@ -20,26 +21,20 @@ class DatabaseInstaller {
         // Read the SQL file
         $sql = file_get_contents($this->sql_file_path);
     
-        // Replace the old prefix with the current WordPress table prefix
-        $old_prefix = 'uqh_';  // Replace with the prefix from bravefactor.sql
-        $sql = str_replace($old_prefix, $this->wpdb->prefix, $sql);
-    
         // Split SQL into individual queries
         $queries = explode(';', $sql);
     
-        // Execute the adjusted queries, excluding wp_users and wp_usermeta
-        try {
-            foreach ($queries as $query) {
-                // Check if the query is related to users or usermeta table
-                if (strpos($query, $this->wpdb->prefix . 'users') === false && strpos($query, $this->wpdb->prefix . 'usermeta') === false && trim($query)) {
-                    $this->wpdb->query($query);
-                }
+        // Execute each query
+        foreach ($queries as $query) {
+            if (trim($query) && 
+                strpos($query, $this->wpdb->prefix . 'users') === false && 
+                strpos($query, $this->wpdb->prefix . 'usermeta') === false) {
+                
+                $this->wpdb->query($query);
             }
-        } catch (Exception $e) {
-            return "Error executing SQL: " . $e->getMessage();
         }
     
-        return "Database restored successfully with wp_users and wp_usermeta excluded!";
-    }
+        return "Database restored successfully with exclusions!";
+    }    
     
 }
